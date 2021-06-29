f40dbd9b9b9d1387e3544455a8f3a6217096baea
zhucong <a1249646457@qq.com>
2021-06-29 14:16:50 +0800

    添加 util工具使用

--- util.go
+++ util.go
@@ -3,6 +3,7 @@ package mgorm
 import (
 	"context"
 	"fmt"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"time"
 )
 
@@ -18,4 +19,17 @@ func Context() context.Context {
 
 func executeStatement(colName string, filter, update interface{}) string {
 	return fmt.Sprintf("colName:%s - filter:%v - update :%v", colName, filter, update)
+}
+
+func IsObjectIdHex(s string) bool {
+	_, err := primitive.ObjectIDFromHex(s)
+	return err == nil
+}
+
+func ObjectIdHex(s string) primitive.ObjectID {
+	id, err := primitive.ObjectIDFromHex(s)
+	if err != nil {
+		return primitive.ObjectID{}
+	}
+	return id
 }
